773a1b9f0da21bfe9f43f99b15eeff630d861968
Matthew Hu <matthu24@gmail.com>
2018-02-23 11:44:04 -0800

    Add reducer

---
+++ frontend/reducers/entities_reducer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {combineReducers} from 'redux';
+import colorsReducer from './colors_reducer';
+
+
+
+
+export default combineReducers({
+  colors: colorsReducer,
+
+});
---
+++ frontend/store/store.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers/root';
+import thunk from '../thunk/thunk';
+
+
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
+const configureStore = (preloadedState = {}) => (
+  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares))
+);
+
+//
+
+export default configureStore;
