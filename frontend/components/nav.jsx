0d6b4e598b87d1f69c8b6410c5b6880666905ea4
Matthew Hu <matthu24@gmail.com>
2018-02-23 12:14:17 -0800

    Add nav bar

--- frontend/components/nav.jsx
+++ frontend/components/nav.jsx
@@ -6,13 +6,13 @@ class Nav extends React.Component{
   }
   render(){
     // var randomColor = Math.floor(Math.random()*16777215).toString(16);
-    // <img src="logo-symbol.svg" className='logo'/>
 
     return(
       <div className='nav'>
 
         <div className='nav-left'>
-          H
+          <img src="https://s3.us-east-2.amazonaws.com/personalwebsite1/HelpfulHumanLogo.png" className='logo'/>
+
         </div>
 
         <div className='nav-right'>
