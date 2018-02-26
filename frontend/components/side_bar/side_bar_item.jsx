226f94c8eeaed8ecab8dd5647bb24031641bd081
Matthew Hu <matthu24@gmail.com>
2018-02-26 00:07:00 -0800

    Add hover style to side bar

--- frontend/components/side_bar/side_bar_item.jsx
+++ frontend/components/side_bar/side_bar_item.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default props => {
   return(
-    <li >
+    <li className='side-bar-item'>
       <Link to={`/colors/${props.color.id}`}>
         {props.color.name}
       </Link>
