c94dda315ca050adf6a71decf5555aafad0b5ac5
Matthew Hu <matthu24@gmail.com>
2018-02-26 12:38:56 -0800

    Add logger to production

--- frontend/components/group/group.jsx
+++ frontend/components/group/group.jsx
@@ -8,27 +8,28 @@ class Group extends React.Component{
   }
 
   componentDidMount(){
-    this.props.fetchSingleGroup(15);
+    this.props.fetchAllGroups();
   }
 
   render(){
     if(!this.props.groups[0]) return null;
-      console.log(this.props.groups[0].name)
-
-      let hexes = Object.values(this.props.groups[0].hexes)
+      // console.log(this.props.groups[0].name)
+      //
+      // let hexes = Object.values(this.props.groups[0].hexes)
 
     return(
       <div className='group1-index'>
-        <div>{this.props.groups[0].name}</div>
-        <ul>
-          {
-            hexes.map((hex,id)=><li>{hex.code}</li>)
-          }
-        </ul>
-
+        <div>hi</div>
       </div>
     )
   }
 }
 
 export default Group;
+//
+// <div>{this.props.groups[0].name}</div>
+// <ul>
+//   {
+//     hexes.map((hex,id)=><li>{hex.code}</li>)
+//   }
+// </ul>
